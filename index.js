ea50fbf330247880172a4200a296337893afe8b7
karolwasemann <kw.kwaq@gmail.com>
2022-06-07 09:42:06 +0200

    task solved

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 // Import stylesheets
 import './style.css';
 
+const list = document.querySelectorAll('.list>div');
+const searchInput = document.querySelector('input[type=text]');
+
+searchInput.addEventListener('keyup', () => {
+  let search = searchInput.value.toLowerCase();
+
+  for (let el of list) {
+    let item = el.querySelector('.contact').innerHTML.toLowerCase();
+
+    if (item.indexOf(search) == -1) {
+      el.classList.remove('show-animation');
+      el.classList.add('hide-animation');
+      setTimeout(() => {
+        el.classList.add('hide');
+      }, 500);
+    } else {
+      el.classList.remove('hide-animation');
+      if (el.classList.contains('hide')) {
+        el.classList.add('show-animation');
+        setTimeout(() => el.classList.remove('hide'), 500);
+      }
+    }
+  }
+});
